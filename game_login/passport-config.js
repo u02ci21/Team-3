b7d51ae81b18027a251016a7619918de558496a3
u03pk23 <u03pk23@abdn.ac.uk>
2025-11-05 14:45:58 +0000

    fixed done() error within the passport authentication

--- game_login/passport-config.js
+++ game_login/passport-config.js
@@ -5,21 +5,22 @@ const bcrypt = require('bcrypt')
 function initialize(passport, getUserByEmail, getUserById) {
     //Function to authenticate users
     const authenticateUser = async (email, password, done) => {
-        //Get user by email
-        const user = getUserByEmail(email)
-        if (user == null) {
-            return done(null, false, { message: 'No user found with that email' })
-        }
-        try {
-            if (await bcrypt.compare(password, user.password)){
-                return done(null, user)
-            } else {
-                return done(null, false, {message: 'Password incorrect'})
-            }
-        } catch (e) {
-            console.log(e);
-            return done(e)
-        }
+
+        getUserByEmail = (email, (error, user) => {
+            if (error) 
+                return done(error);
+            if (!user)
+                return done(null, false, { message: 'No user found with that email' });
+
+            bycrypt.compare(password, user.password, (error, isMatch) => {
+                if (error) 
+                    return done(error);
+                if (isMatch) 
+                    return done(null, user);
+                else 
+                    return done(null, false, { message: 'Password incorrect' });
+            });
+        });
     }
 
     passport.use(new localStrategy({ usernameField: 'email' }, authenticateUser))
