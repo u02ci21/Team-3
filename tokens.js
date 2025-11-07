3095ae193ffa23d6e083f55e6c3318602b39531b
BasWilliams1 <159037659+BasWilliams1@users.noreply.github.com>
2025-11-07 10:37:00 +0000

    Update tokens.js
    Tokens added and functions

--- tokens.js
+++ tokens.js
@@ -1,1 +1,26 @@
+let tokens = Number(localStorage.getItem("tokens")) || 0;
 
+function saveTokens() {
+  localStorage.setItem("tokens", tokens);
+}
+
+function addTokens(amount) {
+  tokens += amount;
+  saveTokens();
+}
+
+function spendTokens(amount) {
+  if (tokens >= amount) {
+    tokens -= amount;
+    saveTokens();
+    return true;
+  }
+  return false;
+}
+
+
+
+function resetTokens() {
+  tokens = 0;
+  saveTokens();
+}
